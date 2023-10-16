e97658f9b806e2700d1820e8e366d857639e7113
Masznyik Márton <eper.karma@gmail.com>
2023-10-16 17:17:20 +0200

    Keves haladas a file beolvasas helyen

--- SimpleATM/SimpleATM/Form2.cs
+++ SimpleATM/SimpleATM/Form2.cs
@@ -8,12 +8,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SimpleATM
 {
     public partial class Form2 : Form
     {
-        
+        public struct BankAccount
+        {
+            public int cardNumber;
+            public string cardHolder;
+            public string expirationDate;
+            public int cvc;
+        }
+        public List<BankAccount> bankAccounts = new List<BankAccount>(5);
         public Form2()
         {
             InitializeComponent();
@@ -40,11 +48,30 @@ namespace SimpleATM
 
         private void submitbutton_Click(object sender, EventArgs e)
         {
+            /*
             TextWriter tw = new StreamWriter("BankAccounts.txt");
             String text = textBox1.Text;
             tw.WriteLine(text);
             label10.Text = text;
             tw.Close();
+            */
+            FileStream f = new FileStream("BankAccounts.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(f, Encoding.UTF8);
+            BankAccount aktualis = new BankAccount();
+            while (!sr.EndOfStream) 
+            {
+                string[] sor = sr.ReadLine().Split(' ');
+                aktualis.cardNumber = Convert.ToInt32(sor[0]);
+                aktualis.cardHolder = sor[1];
+                aktualis.cardHolder += sor[2];
+                aktualis.expirationDate = sor[3];
+                aktualis.cvc = Convert.ToInt32(sor[4]);
+                bankAccounts.Add(aktualis);
+            }
+            label10.Text = Convert.ToString(bankAccounts[0].cardNumber);
+            label11.Text = Convert.ToString(bankAccounts[0].cardHolder); 
+            label12.Text = Convert.ToString(bankAccounts[0].expirationDate); 
+            label13.Text = Convert.ToString(bankAccounts[0].cvc); 
         }
     }
 }
